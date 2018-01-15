8e11c567b912cd239b208b2e4dc61b8ddc257b89
souadfawzy <souad.fawzy@gmail.com>
2018-01-15 22:25:53 +0400

    add validator

---
+++ CognitWatsonAssessment/src/main/java/com/cognit/watson/nlc/utilities/Validator.java
@@ -0,0 +1,20 @@
+package com.cognit.watson.nlc.utilities;
+
+/**
+ * @author Souad
+ *
+ */
+public class Validator {
+
+	/**
+	 * check if value is empty or null
+	 * 
+	 * @param value
+	 * @return true if value is empty or null ,false otherwise
+	 */
+	public static boolean isEmptyOrNull(String value) {
+
+		return (value == null || value.isEmpty());
+
+	}
+}
